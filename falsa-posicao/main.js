b7a2586388d3b304ae3be7da7236e63b2c9e24cd
Larissa <63196108+laripereira14@users.noreply.github.com>
2023-10-31 09:17:38 -0300

    Update main.js

--- falsa-posicao/main.js
+++ falsa-posicao/main.js
@@ -10,7 +10,7 @@ function false_position(equation, a, b) {
 	let interaction = 0;
 	let results = [];
 
-	while (b - a > epsilon && interaction < 101) {
+	while (b - a > epsilon && interaction < 76) {
 		let fa = solve(a, equation);
 		let fb = solve(b, equation);
 		let xk = (a * fb - b * fa) / (fb - fa);
